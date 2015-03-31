51da7cd0559b6040f8636fba0019cf809d26e982
dempeZheng <752996510@qq.com>
2015-03-31 23:30:37 +0800

    init

---
+++ poplar-keeper/src/main/java/com/dempe/poplar/keeper/ConfMananger.java
@@ -0,0 +1,12 @@
+package com.dempe.poplar.keeper;
+
+/**
+ * 配置管理类
+ * 提供zookeeper写入配置文件
+ * User: zhengdaxia
+ * Date: 15/3/31
+ * Time: 下午11:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConfMananger {
+}
