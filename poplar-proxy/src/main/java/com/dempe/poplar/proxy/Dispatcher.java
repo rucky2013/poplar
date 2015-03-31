652c70ed1e34d47fe020033b07bf1d6e01a6dee0
dempeZheng <752996510@qq.com>
2015-03-31 22:32:11 +0800

    init

---
+++ poplar-proxy/src/main/java/com/dempe/poplar/proxy/Dispatcher.java
@@ -0,0 +1,11 @@
+package com.dempe.poplar.proxy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhengdaxia
+ * Date: 15/3/31
+ * Time: 下午9:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class Dispatcher {
+}
