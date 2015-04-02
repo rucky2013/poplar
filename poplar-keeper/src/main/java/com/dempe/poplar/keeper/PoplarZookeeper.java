1699bea21fb9e6a77b85f53ffa0e63ab6265f9e1
DempeZheng <752996510@qq.com>
2015-04-02 20:22:32 +0800

    init

---
+++ poplar-keeper/src/main/java/com/dempe/poplar/keeper/PoplarZookeeper.java
@@ -0,0 +1,32 @@
+package com.dempe.poplar.keeper;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dempe
+ * Date: 2015/4/2
+ * Time: 19:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class PoplarZookeeper {
+
+    public static void main(String[] args) throws Exception {
+
+        CuratorFramework client = CuratorFrameworkFactory.builder().connectString("120.24.211.40:2180")
+                .sessionTimeoutMs(30000)
+                .connectionTimeoutMs(30000)
+                .canBeReadOnly(false)
+                .retryPolicy(new ExponentialBackoffRetry(1000, Integer.MAX_VALUE))
+                .namespace("poplar")
+                .defaultData(null)
+                .build();
+        client.start();
+
+        System.out.println("-----------------");
+        client.create().forPath("servers", "localhost:8080".getBytes());
+        System.out.println(client.getData().forPath("servers"));
+    }
+}
